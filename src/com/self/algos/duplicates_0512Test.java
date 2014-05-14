bc4f6eae09b1beec69b868d72394209cb10b4bf6
Dinesh Deiveegan <ddeiveegan@sfmacbook10.local>
2014-05-14 11:53:50 -0700

    Adding more test cases

--- src/com/self/algos/duplicates_0512Test.java
+++ src/com/self/algos/duplicates_0512Test.java
@@ -21,21 +21,55 @@ public class duplicates_0512Test {
 	 */
 	@Before
 	public void setUp() throws Exception {
-		int[] input = {1,1,1,3,4,5,5,6};	
 		duplicates = new duplicates_0512();
-		duplicates.setInputArray(input);
 	}
 
 	@Test
-	public void test() {
+	public void test1() {
+		int[] input = {1,1,1,3,4,5,5,6};
 		int[] output = {1,3,4,5,6};
+		duplicates.setInputArray(input);
 		duplicates.removeDuplicates();
 		int[] actualOutput = duplicates.getOutputArray();
 		for(int iter=0; iter < output.length; iter ++) {
 			assertEquals(actualOutput[iter],output[iter]);
 		}
-		//assertEquals(duplicates.getOutputArray(),output);
-		//fail("Not yet implemented");
+	}
+	
+	@Test
+	public void test2() {
+		int[] input = {};
+		int[] output = {};
+		duplicates.setInputArray(input);
+		duplicates.removeDuplicates();
+		int[] actualOutput = duplicates.getOutputArray();
+		for(int iter=0; iter < output.length; iter ++) {
+			assertEquals(actualOutput[iter],output[iter]);
+		}
+	}
+	
+	@Test
+	public void test3() {
+		int[] input = {1,1,1,1,1,1,1};
+		int[] output = {1};
+		duplicates.setInputArray(input);
+		duplicates.removeDuplicates();
+		int[] actualOutput = duplicates.getOutputArray();
+		for(int iter=0; iter < output.length; iter ++) {
+			assertEquals(actualOutput[iter],output[iter]);
+		}
+	}
+	
+	@Test
+	public void test4() {
+		int[] input = {-1,-1,0,1,1,3,2,2,5};
+		int[] output = {-1,0,1,2,3,5};
+		duplicates.setInputArray(input);
+		duplicates.removeDuplicates();
+		int[] actualOutput = duplicates.getOutputArray();
+		for(int iter=0; iter < output.length; iter ++) {
+			assertEquals(actualOutput[iter],output[iter]);
+		}
 	}
 
 }
